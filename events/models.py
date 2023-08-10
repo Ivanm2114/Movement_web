542e9b62e87c0d92bea96164c3e4706b8ddee1eb
Ivan <67190437+Ivanm2114@users.noreply.github.com>
2023-08-10 19:11:30 +0700

    Head, Sponsor, Video added Events, Team eddited

--- events/models.py
+++ events/models.py
@@ -1,6 +1,35 @@
 from django.db import models
 
 
+
+class Photo(models.Model):
+    image = models.ImageField()
+    title = models.CharField(max_length=200)
+
+
+class Video(models.Model):
+    video = models.FileField()
+    title = models.CharField(max_length=200)
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    short_description = models.CharField(max_length=2000)
+    long_description = models.CharField(max_length=2000)
+    start_date = models.DateTimeField()
+    start_year = models.IntegerField()
+    end_date = models.DateTimeField()
+    end_year = models.IntegerField()
+    poster = models.ImageField()
+    video = models.FileField()
+    trailer = models.FileField()
+    amount_of_members = models.IntegerField()
+    recent = models.BooleanField()
+    photos = models.ManyToManyField(Photo)
+
+    def __str__(self):
+        return self.name + ' ' + str(self.start_date.year)
+
 class Person(models.Model):
     name = models.CharField(max_length=200)
     surname = models.CharField(max_length=200)
@@ -8,6 +37,7 @@ class Person(models.Model):
     phone = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     where_knew = models.CharField(max_length=200)
+    telegram = models.CharField(max_length=200)
 
     def __str__(self):
         return self.surname + ' ' + self.name + ' ' + self.fathers_name
@@ -21,29 +51,27 @@ class PersonNotHSE(models.Model):
         return self.person.surname + ' ' + self.person.name + ' ' + self.person.fathers_name
 
 
-class Event(models.Model):
-    name = models.CharField(max_length=200)
-    short_description = models.CharField(max_length=2000)
-    long_description = models.CharField(max_length=2000)
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-    poster = models.ImageField()
-    telegram_URL = models.URLField()
-
-    def __str__(self):
-        return self.name + ' ' + str(self.start_date.year)
-
-
 class Team(models.Model):
     name = models.CharField(max_length=200)
     captain = models.ForeignKey(Person, on_delete=models.DO_NOTHING)
     members = models.CharField(max_length=500)
     event = models.ForeignKey(Event, on_delete=models.DO_NOTHING)
+    amount_of_members = models.IntegerField()
 
     def __str__(self):
         return self.name + ' ' + self.event.name
 
 
-class Photo(models.Model):
-    image = models.ImageField()
-    event = models.ForeignKey(Event, on_delete=models.DO_NOTHING)
+
+
+class Sponsor(models.Model):
+    name = models.CharField(max_length=200)
+    logo = models.ImageField()
+    url = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+
+class Head(Person):
+    photo = models.ImageField()
