6a75229447818f0ec4972fb6c5aeb73ee6645e9b
Ivan <67190437+Ivanm2114@users.noreply.github.com>
2023-08-12 12:17:28 +0300

    PhotoAlbum added

--- events/models.py
+++ events/models.py
@@ -1,30 +1,44 @@
 from django.db import models
 
 
+class PhotoAlbum(models.Model):
+    name = models.CharField(max_length=200)
+
+
 class Photo(models.Model):
-    image = models.ImageField()
+    image = models.ImageField(blank=True)
+    album = models.ForeignKey(PhotoAlbum, null=True, on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=200)
+
+    def __str__(self):
+        if self.title:
+            return self.title
+        return self.image.name
 
 
 class Video(models.Model):
-    video = models.FileField()
+    video = models.FileField(blank=True)
     title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
 
 
 class Event(models.Model):
     name = models.CharField(max_length=200)
     short_description = models.CharField(max_length=2000)
     long_description = models.CharField(max_length=2000)
-    start_date = models.DateTimeField()
-    start_year = models.IntegerField()
-    end_date = models.DateTimeField()
-    end_year = models.IntegerField()
-    poster = models.ImageField()
-    video = models.FileField()
-    trailer = models.FileField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    poster = models.ForeignKey(Photo, on_delete=models.DO_NOTHING, blank=True, related_name='poster', null=True)
+    video = models.ForeignKey(Video, on_delete=models.DO_NOTHING, blank=True, related_name='video_report', null=True)
+    trailer = models.ForeignKey(Video, on_delete=models.DO_NOTHING, blank=True, related_name='trailer', null=True)
     amount_of_members = models.IntegerField()
     recent = models.BooleanField()
-    photos = models.ManyToManyField(Photo)
+    photo_album = models.ForeignKey(PhotoAlbum, null=True, on_delete=models.DO_NOTHING)
+    registration_url = models.URLField(blank=True)
+    status = models.CharField(max_length=50)
+    partners_event = models.BooleanField()
 
     def __str__(self):
         return self.name + ' ' + str(self.start_date.year)
@@ -34,9 +48,8 @@ class Person(models.Model):
     name = models.CharField(max_length=200)
     surname = models.CharField(max_length=200)
     fathers_name = models.CharField(max_length=200)
-    phone = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
-    where_knew = models.CharField(max_length=200)
+    where_knew = models.CharField(max_length=200, blank=True)
     telegram = models.CharField(max_length=200)
 
     def __str__(self):
@@ -64,7 +77,7 @@ class Team(models.Model):
 
 class Sponsor(models.Model):
     name = models.CharField(max_length=200)
-    logo = models.ImageField()
+    logo = models.ForeignKey(Photo, on_delete=models.DO_NOTHING)
     url = models.URLField()
 
     def __str__(self):
@@ -72,6 +85,5 @@ class Sponsor(models.Model):
 
 
 class Head(Person):
-    photo = models.ImageField()
+    photo = models.ForeignKey(Photo, on_delete=models.DO_NOTHING)
     position = models.CharField(max_length=200)
-
