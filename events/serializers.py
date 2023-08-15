107c21f18fbaa98ee2604b6edcc59f1b9787819e
Ivan <67190437+Ivanm2114@users.noreply.github.com>
2023-08-15 16:14:38 +0700

    edited Photos field also returns their url in json, PhotoAlbum returns all photos inside it

--- events/serializers.py
+++ events/serializers.py
@@ -4,17 +4,29 @@ from .models import Event, Person, PersonNotHSE, Photo, Team, Sponsor, Head, Pho
 
 
 class PhotoAlbumSerializer(serializers.HyperlinkedModelSerializer):
+    photos = serializers.SerializerMethodField('get_album_photos')
+
+    def get_album_photos(self, obj):
+        return list(x.image.url for x in Photo.objects.filter(album=obj))
     class Meta:
         model = PhotoAlbum
-        fields = ['id','name']
+        fields = ['id', 'name', 'photos']
 
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
+    poster_url = serializers.SerializerMethodField('get_poster_url')
+
+    def get_poster_url(self, obj):
+        if obj.poster:
+            return obj.poster.image.url
+        else:
+            return None
+
     class Meta:
         model = Event
         fields = ['id', 'name',
                   'short_description', 'long_description',
-                  'start_date', 'end_date', 'poster',
+                  'start_date', 'end_date', 'poster', 'poster_url',
                   'video', 'trailer',
                   'amount_of_members', 'recent',
                   'photo_album', 'registration_url',
@@ -53,16 +65,32 @@ class PhotoSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class SponsorSerializer(serializers.HyperlinkedModelSerializer):
+    logo_url = serializers.SerializerMethodField('get_logo_url')
+
+    def get_logo_url(self, obj):
+        if obj.logo:
+            return obj.logo.image.url
+        else:
+            return None
+
     class Meta:
         model = Sponsor
         fields = ['id', 'name',
-                  'logo', 'site_link']
+                  'logo', 'logo_url', 'site_link']
 
 
 class HeadSerializer(serializers.HyperlinkedModelSerializer):
+    photo_url = serializers.SerializerMethodField('get_photo_url')
+
+    def get_photo_url(self, obj):
+        if obj.photo:
+            return obj.photo.image.url
+        else:
+            return None
+
     class Meta:
         model = Head
         fields = ['id', 'name',
                   'surname', 'fathers_name',
                   'email', 'where_knew',
-                  'telegram', 'photo', 'position']
+                  'telegram', 'photo', 'photo_url', 'position']
