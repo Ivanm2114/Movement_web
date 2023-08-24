766fdeea64c9da218e1b15d392ee72be9d6ae783
Ivan <67190437+Ivanm2114@users.noreply.github.com>
2023-08-24 15:45:53 +0700

    security added

--- Movement_web/settings.py
+++ Movement_web/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0qz$r(2bt(h%vax4z$7f$k+ymh80-4v0flb^6olhgm$incnq4^'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
